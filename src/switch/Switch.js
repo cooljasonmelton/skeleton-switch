d1722610c3ed0214b318edaf6bcd8783ee4a5bf0
cooljasonmelton <cooljasonmelton>
2020-08-19 01:06:05 -0500

    built and styled skull switch

--- src/switch/Switch.js
+++ src/switch/Switch.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import skull from './skull-emoji.png'
+
+// styling
 import './Switch.css';
 
 const Switch = () => {
-  return (
-    <div className="Switch cfb">  
-        <div className="switch-track"></div>
-        <div className="switch-circle">test</div>
-    </div>
-  );
+    const [switchOn, setSwitchOn] = useState(false)
+
+    const setCircleClass = () => {
+        const classArr = ["switch-circle"]
+        if (switchOn) classArr.push('switch-right')
+        return classArr.join(' ')
+    }
+
+    const setSkullClass = () => {
+        const classArr = ["skull"]
+        if (switchOn) classArr.push('skull-on')
+        return classArr.join(' ')
+    }
+
+    return (
+        <div className="Switch cfb">  
+            <div className="switch-track"></div>
+            <div onClick={()=>setSwitchOn(!switchOn)} className={setCircleClass()}>
+                <img src={skull} className={setSkullClass()}/>
+            </div>
+        </div>
+    );
 }
 
 export default Switch;
